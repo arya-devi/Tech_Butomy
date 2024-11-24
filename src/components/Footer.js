9560eca7df9df68f421608b7a7f6bf25577dc528
Arya Devi <aryaarya96016@gmail.com>
2024-11-24 19:58:12 +0530

    ..

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -68,7 +68,7 @@ const Footer = () => {
                   />
                   <button
                     type="submit"
-                    className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+                    className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded ml-2"
                   >
                     Subscribe
                   </button>
